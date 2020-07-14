97795b667f1b9674f240eb73f9a2c7cd4094c02b
danielpdwalker <danielpdwalker@gmail.com>
2020-07-14 09:32:08 +0100

    added leaderboard link

--- game/urls.py
+++ game/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, urls
 from . import views
 
 urlpatterns = [
     path('', views.game, name='game'),
-    path('leaderboard/', views.leaderboard, name='leaderboard')
+    path('leaderboard/', views.leaderboard, name='leaderboard'),
+    #url(r'^ajax/post/$', '')
+
 ]
