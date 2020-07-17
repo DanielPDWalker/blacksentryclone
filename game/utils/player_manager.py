ebee74023899601edc6ad548a95d51ef8de746f6
danielpdwalker <danielpdwalker@gmail.com>
2020-07-17 13:51:47 +0100

    Added random username generation.

--- game/utils/player_manager.py
+++ game/utils/player_manager.py
@@ -1,40 +1,39 @@
 from ..models import Player
 
-
-## Replace all this username stuff with "pip install random-username"
 import random
-import csv
+import os
 
 
-f_names = list(csv.reader('first_names.csv'))
-s_names = list(csv.reader('second_names.csv'))
+def get_random_username():
+    base_dir = os.path.dirname(__file__)
+    adjectives = []
+    nouns = []
 
+    with open(os.path.join(base_dir, 'names/adjectives.txt'), 'r') as a_file:
+        for name in a_file:
+             adjectives.append(name.strip())
+
+    with open (os.path.join(base_dir, 'names/nouns.txt'), 'r') as n_file:
+        for name in n_file:
+            nouns.append(name.strip())
+
+    username = random.choice(adjectives).title() + ' ' + random.choice(nouns).title() + str(random.randrange(1000))
+
+    if username in Player.objects.filter(name=username):
+        get_random_username()
+    else:
+        return username
 
 def init_player(player_obj):
-    
-    random_number = random.randrange(0, len(f_names))
-    name_picked = f_names[random_number] + ' '
+    username = get_random_username()
 
-    random_number = random.randrange(0, len(s_names))
-    name_picked  += s_names[random_number]
-    if name_picked in Player.objects.filter(name=name_picked):
-        init_player(player_obj)
-    else:
-        p = Player(player_obj)
-        p.name = name_picked
-        p.save()
-
+    p = Player(player_obj)
+    p.name = username
+    p.save()
 
 
 def change_name(player_obj):
-    random_number = random.randrange(0, len(f_names))
-    name_picked = str(f_names[random_number]) + ' '
+    username = get_random_username()
 
-    random_number = random.randrange(0, len(s_names))
-    name_picked  += str(s_names[random_number])
-
-    if name_picked in Player.objects.filter(name=name_picked):
-        change_name(player_obj)
-    else:
-        player_obj.name = name_picked
-        player_obj.save()
+    player_obj.name = username
+    player_obj.save()
