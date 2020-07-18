e3ae96ce0a8db76c9d9ba9b6a4c91d89346da367
danielpdwalker <danielpdwalker@gmail.com>
2020-07-18 14:04:33 +0100

    Added online and offline tests, added html IDs for testing

--- functional_tests/online_registration_tests.py
+++ functional_tests/online_registration_tests.py
@@ -1,17 +1,17 @@
-from django.test import LiveServerTestCase
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
 
 
-from .local_test_settings import testpassword1, test_host
+from local_test_settings import testpassword1, test_live_host
 
 
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(unittest.TestCase):
 
     username1 = 'edythe'
     email1 = 'edythe@gmail.com'
@@ -19,6 +19,7 @@ class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.get(test_live_host)
 
     def tearDown(self):
         self.browser.quit()
@@ -30,7 +31,6 @@ class NewVisitorTest(LiveServerTestCase):
         register = self.browser.find_element_by_id('register_button')
         register.send_keys(Keys.ENTER)
 
-        #Find better way to wait untill page loads?
         time.sleep(2)
 
         username = self.browser.find_element_by_id('id_username')
@@ -47,7 +47,6 @@ class NewVisitorTest(LiveServerTestCase):
 
 
     def log_in(self):
-        #Find better way to wait untill page loads?
         time.sleep(2)
 
         username = self.browser.find_element_by_id('id_username')
@@ -61,19 +60,14 @@ class NewVisitorTest(LiveServerTestCase):
     def test_signup(self):
         # Edythe has heard of this hip new game called "I DONT KNOW HOW TO NAME THINGS".
         # She decides to sign up.
-        self.browser.get(host)
 
         self.sign_up()
 
+        # She immediately logs in.
         self.log_in()
-
-        time.sleep(3)
-
-        
-
-        
-    
+        time.sleep(2)
+        self.assertIn('edythe', self.browser.find_element_by_id('logged_in'))
 
 
-
-        self.fail('Finish the signup test!')
+if __name__ == "__main__":
+    unittest.main()
